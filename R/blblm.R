071398058bad3838d10d48a818f44e2723742f91
Randy Lai <randy.cs.lai@gmail.com>
2020-05-24 23:20:03 -0700

    add utils::globalVariables

--- R/blblm.R
+++ R/blblm.R
@@ -5,6 +5,12 @@
 #' Linear Regression with Little Bag of Bootstraps
 "_PACKAGE"
 
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+# from https://github.com/jennybc/googlesheets/blob/master/R/googlesheets.R
+utils::globalVariables(c("."))
+
+
 #' @export
 blblm <- function(formula, data, m = 10, B = 5000) {
   data_list <- split_data(data, m)
